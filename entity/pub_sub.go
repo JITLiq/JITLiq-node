3a12f0da6a93d4966f2cf7150ffd846e4ff8ea09
Rahul Tripathi <rahultripathidev@gmail.com>
2024-09-21 02:09:09 +0800

    ft: add new entities

--- entity/pub_sub.go
+++ entity/pub_sub.go
@@ -8,10 +8,15 @@ const (
 	ORDER_PUBSUB_TOPIC = "jit-liq-order"
 )
 
+type Order struct {
+	Filler common.Address
+	Amount JsonBigInt
+}
+
 type AttestOrderPayload struct {
-	OrderID         string         `json:"orderId"`
-	Operator        common.Address `json:"operator"`
-	Signature       string         `json:"signature"`
-	ChainID         int64          `json:"chainId"`
-	TransactionHash common.Hash    `json:"transactionHash"`
+	OrderID   string         `json:"orderId"`
+	Operator  common.Address `json:"operator"`
+	Signature string         `json:"signature"`
+	ChainID   int64          `json:"chainId"`
+	Orders    []Order        `json:"orders"`
 }
