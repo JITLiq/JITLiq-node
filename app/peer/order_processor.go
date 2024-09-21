126ee9c01763d6d5379e66ea8895458532637199
Rahul Tripathi <rahultripathidev@gmail.com>
2024-09-21 16:41:42 +0800

    ft: add relayer to order processor

--- app/peer/order_processor.go
+++ app/peer/order_processor.go
@@ -2,11 +2,11 @@ package peer
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/JITLiq/node/config"
 	"github.com/JITLiq/node/internal/services/attestation"
 	"github.com/JITLiq/node/internal/services/reader"
+	"github.com/JITLiq/node/internal/services/relayer"
 	"github.com/JITLiq/node/internal/services/solver"
 	"github.com/JITLiq/node/pkg/logger"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -24,6 +24,7 @@ func processOrders(
 	cfg *config.Config,
 	solver *solver.Solver,
 	aggregator *attestation.Aggregator,
+	relayer *relayer.OrderRelayer,
 ) {
 	log := logger.NewLogger("OrderProcessor")
 	client, err := ethclient.Dial(cfg.ArbRPCURL)
@@ -44,7 +45,7 @@ func processOrders(
 		case <-ctx.Done():
 			return
 		case l := <-sub.Read():
-			err = solveAndPrepareForRelay(ctx, l, solver, aggregator)
+			err = solveAndRelay(ctx, l, solver, aggregator, relayer)
 			if err != nil {
 				log.Error("failed to solve order", zap.Error(err))
 			}
@@ -52,14 +53,15 @@ func processOrders(
 	}
 }
 
-func solveAndPrepareForRelay(
+func solveAndRelay(
 	ctx context.Context,
 	log types.Log,
 	solver *solver.Solver,
 	aggregator *attestation.Aggregator,
+	relayer *relayer.OrderRelayer,
 ) error {
 	if len(log.Topics) < 2 {
-		return errors.New("topics too smol")
+		return errors.New("topic too smol")
 	}
 	orderID := log.Topics[1]
 	payload, err := solver.Solve(ctx, orderID)
@@ -72,6 +74,10 @@ func solveAndPrepareForRelay(
 		return err
 	}
 
-	fmt.Println(attestations)
+	_, err = relayer.RelayOrder(ctx, attestations)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
