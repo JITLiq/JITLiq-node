1b6d9d83edb24a57ed05305f4aad6770b71ab951
Rahul Tripathi <rahultripathidev@gmail.com>
2024-09-21 16:39:15 +0800

    ft: update addresses

--- entity/addresses.go
+++ entity/addresses.go
@@ -8,8 +8,9 @@ const (
 )
 
 var (
-	TopicOrderCreated = common.HexToHash("0x918554b6bd6e2895ce6553de5de0e1a69db5289aa0e4fe193a0dcd1f14347477")
-	BaseUsdc          = common.HexToAddress("0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913")
-	TargetJITPool     = common.HexToAddress("")
-	SrcStateManager   = common.HexToAddress("0x7c62010e5CC7A839826A50a541bf20767750d345")
+	TopicOrderCreated  = common.HexToHash("0x918554b6bd6e2895ce6553de5de0e1a69db5289aa0e4fe193a0dcd1f14347477")
+	BaseUsdc           = common.HexToAddress("0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913")
+	TargetJITPool      = common.HexToAddress("0x5eB42e519408ec6E2e76928Aac347352d2375904")
+	TargetStateManager = common.HexToAddress("0xFE4A47a399A76db4dd98bbfb6913C2cC3d585BAf")
+	SrcStateManager    = common.HexToAddress("0x6dA38F0A1EB71cD8279F073604c859eA67a50D6F")
 )
